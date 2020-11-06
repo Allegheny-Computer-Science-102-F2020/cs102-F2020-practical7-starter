cc056aa599b8884c1e9b80818a7e092743f2ba15
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-11-06 08:36:23 -0500

    Add the cartesian module with the empty cartesian_product with TODO.

---
+++ cartesiancreator/cartesiancreator/cartesian.py
@@ -0,0 +1,11 @@
+"""Compute the Cartesian product of two sets."""
+
+from typing import Iterator
+from typing import Set
+
+
+def cartesian_product(first: Set[int], second: Set[int]) -> Iterator[Set[int]]:
+    """Compute and return the Cartesian product of two sets."""
+    # TODO: iterate through all of the elements in the first set
+    # TODO: iterator through all of the elements in the second set
+    # TODO: create a set that contains those elements and yield it
